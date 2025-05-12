2aef87ea4537ea94c5d05899a1fc416029279ffa
Geek Neuron <geekneuron@gmail.com>
2025-05-12 23:42:01 +0330

    about_version_patch.cc

---
+++ Script/about_version_patch.cc
@@ -0,0 +1,6 @@
+// about_version_patch.cc
+// This patch can be used in chrome/browser/about_flags or about_version.cc
+
+const char* GetNexaBrowserBrand() {
+    return "Nexa Browser (Powered by Chromium)";
+}
