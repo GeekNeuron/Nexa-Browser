7c09e00e65bda10916fc99dcbd64fb9bdf2e47e2
Geek Neuron <geekneuron@gmail.com>
2025-05-12 23:44:59 +0330

    chrome_main_patch.cc

--- Script/chrome_main_patch.cc
+++ Script/chrome_main_patch.cc
@@ -1,1 +1,8 @@
+// chrome_main_patch.cc
+#include "splash_screen.cc"
+#include "chrome/app/chrome_main.h"
 
+int ChromeMainWithSplash(const content::MainFunctionParams& parameters) {
+    ShowSplash(); // نمایش اسپش
+    return ChromeMain(parameters); // ادامه اجرای اصلی مرورگر
+}
